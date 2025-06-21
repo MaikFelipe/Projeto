ee17485b9efc37c51e00c1533822a810dee534d5
LASEDi 1781 <LASEDi 1781@192.168.10.177>
2025-06-21 18:27:34 -0300

    Primeiro commit

---
+++ src/main/java/com/mycompany/sistemagetaoescolar/SistemaGetaoEscolar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagetaoescolar;
+
+/**
+ *
+ * @author LASEDi 1781
+ */
+public class SistemaGetaoEscolar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
