7dd6ca7e434245ae25333ed89d511899deaf88bd
LASEDi 1781 <LASEDi 1781@192.168.10.177>
2025-06-21 19:19:07 -0300

    Ola brasil

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author LASEDi 1781
+ */
+public class Usuario {
+    private int id;
+    private String nomeCompleto;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String cargo;
+    private String login;
+    private String senha;
+    private int nivelAcesso;
+    
+}
